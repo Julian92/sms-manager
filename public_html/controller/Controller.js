3f159faada014196f91e6daeac25b7973c6b7e18
jerson-suaza <jerson.suaza@gmail.com>
2014-07-15 18:01:58 -0500

    primera version

---
+++ public_html/controller/Controller.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    main();
+});
+
+/**
+ * class controller main app
+ */
+var classController = function(){
+    
+};
+classController.prototype = {
+    
+    loadNavBar: function(){
+        var source   = $("#navbar-template").html();
+        var template = Handlebars.compile(source);
+        var context = {name: "Luis P."};
+        var htmlSrt    = template(context);
+        $('#navBarApp').html(htmlSrt);
+    },
+    
+    getListMessages: function(){
+        
+         var flickerAPI = "http://68.233.227.228/myapps/smsServices/public/sms/getList?jsoncallback=?";
+        $.getJSON(flickerAPI, {
+            tags: "mount rainier",
+            tagmode: "any",
+            format: "json"
+        }).done(function(data) {
+            console.log(data);
+        });
+    },
+    
+    main: function(){
+        this.loadNavBar();
+        this.getListMessages();
+    }
+};
+
+var instanceController = null;
+function main(){
+    instanceController = new classController();
+    instanceController.main();
+}
+
